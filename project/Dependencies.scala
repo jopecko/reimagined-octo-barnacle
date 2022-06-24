6e1f791cea91c3c097f286443475bd3b2062a8f7
Scala Steward <jopecko@users.noreply.github.com>
2022-06-24 11:58:14 -0700

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.0"
 }
